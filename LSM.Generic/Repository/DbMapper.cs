4992a05bd5d5c5b192607693054aa06c19aa90aa
leonardosimoura <leonardosimoura@gmail.com>
2016-07-30 12:02:10 -0300

    Add project files.

---
+++ LSM.Generic/Repository/DbMapper.cs
@@ -0,0 +1,384 @@
+﻿using LSM.Generic.Repository.Attribute;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSM.Generic.Repository
+{
+    public class DbMapper
+    {
+        public T DataRowToObj<T>(DataRow row)
+        {
+            //Obtains the type of the generic class
+            Type t = typeof(T);
+
+            //Obtains the properties definition of the generic class.
+            //With this, we are going to know the property names of the class
+            PropertyInfo[] pi = t.GetProperties();
+
+            object defaultInstance = Activator.CreateInstance(t);
+
+
+            //Create a new instance of the generic class
+            //For each property in the properties of the class
+
+
+            var nomecoluna = "";
+            var mapear = true;
+            foreach (PropertyInfo prop in pi)
+            {
+                try
+                {
+
+
+                    nomecoluna = "";
+                    mapear = true;
+
+                    System.Attribute[] attrs = System.Attribute.GetCustomAttributes(prop);//captura todos os custom Atributes
+
+                    //percorre os atributos
+                    foreach (System.Attribute attr in attrs)
+                    {
+                        if (attr is DbMap)
+                        {
+                            DbMap a = (DbMap)attr;
+                            if (a.Map == true)
+                            {
+                                nomecoluna = a.Coluna;
+                            }
+                            else
+                            {
+                                nomecoluna = "";
+                                mapear = false;
+                            }
+                        }
+                    }
+
+                    if (nomecoluna == "")
+                    {
+                        nomecoluna = prop.Name;
+                    }
+
+                    //Verifica se a coluna existe na DataTable
+                    if (row.Table.Columns.Contains(nomecoluna) && mapear == true)
+                    {
+                        //Get the value of the row according to the field name
+                        //Remember that the classïs members and the tableïs field names
+                        //must be identical
+                        object columnvalue = row[nomecoluna];
+                        //Know check if the value is null. 
+                        //If not, it will be added to the instance
+                        if (columnvalue != DBNull.Value)
+                        {
+                            //Set the value dinamically. Now you need to pass as an argument
+                            //an instance class of the generic class. This instance has been
+                            //created with Activator.CreateInstance(t)
+                            //prop.SetValue(defaultInstance, columnvalue, null);
+
+                            try
+                            {
+                                prop.SetValue(defaultInstance, columnvalue, null);
+                            }
+                            catch (Exception)
+                            {
+
+
+                                if (prop.PropertyType == typeof(bool))
+                                {
+                                    prop.SetValue(defaultInstance, Convert.ToBoolean(columnvalue), null);
+                                }
+                                else if (prop.PropertyType == typeof(int))
+                                {
+                                    prop.SetValue(defaultInstance, Convert.ToInt32(columnvalue), null);
+                                }
+                                else if (prop.PropertyType == typeof(double))
+                                {
+                                    prop.SetValue(defaultInstance, Convert.ToDouble(columnvalue), null);
+                                }
+                                else if (prop.PropertyType == typeof(decimal))
+                                {
+                                    prop.SetValue(defaultInstance, Convert.ToDecimal(columnvalue), null);
+                                }
+                                else if (prop.PropertyType == typeof(float))
+                                {
+                                    prop.SetValue(defaultInstance, float.Parse(columnvalue.ToString()), null);
+                                }
+
+                            }
+                        }
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(prop.Name + ": " + ex.ToString());
+                    //return null;
+                }
+            }
+
+            //Now, create a class of the same type of the generic class. 
+            //Then a conversion itïs done to set the value
+            T myclass = (T)defaultInstance;
+            //Add the generic instance to the generic list
+            return myclass;
+        }
+
+        /// <summary>
+        /// Função responsável por mapear um DataTable para um objeto (o Datatable deve conter apenas 1 DataRow)
+        /// </summary>
+        /// <typeparam name="T">Type/Class para o qual sera mapeado</typeparam>
+        /// <param name="dataTable">Fonte dos dados</param>
+        /// <returns></returns>
+        public T DataTableToObj<T>(DataTable dataTable)
+        {
+
+            if (dataTable.Rows.Count > 1)
+            {
+                throw new Exception("DataTable recebida contem mais de 1 registro.");
+            }
+
+            if (dataTable.Rows.Count == 0)
+            {
+                //return null;
+            }
+
+            //Obtains the type of the generic class
+            Type t = typeof(T);
+
+            //Obtains the properties definition of the generic class.
+            //With this, we are going to know the property names of the class
+            PropertyInfo[] pi = t.GetProperties();
+
+            object defaultInstance = Activator.CreateInstance(t);
+
+            DataRow row = dataTable.Rows[0];
+            //Create a new instance of the generic class
+            //For each property in the properties of the class
+
+            var nomecoluna = "";
+            var mapear = true;
+            foreach (PropertyInfo prop in pi)
+            {
+                try
+                {
+                    nomecoluna = "";
+                    mapear = true;
+
+                    System.Attribute[] attrs = System.Attribute.GetCustomAttributes(prop);//captura todos os custom Atributes
+
+                    //percorre os atributos
+                    foreach (System.Attribute attr in attrs)
+                    {
+                        if (attr is DbMap)
+                        {
+                            DbMap a = (DbMap)attr;
+                            if (a.Map == true)
+                            {
+                                nomecoluna = a.Coluna;
+                            }
+                            else
+                            {
+                                nomecoluna = "";
+                                mapear = false;
+                            }
+                        }
+                    }
+
+                    if (nomecoluna == "")
+                    {
+                        nomecoluna = prop.Name;
+                    }
+
+                    //Verifica se a coluna existe na DataTable
+                    if (dataTable.Columns.Contains(nomecoluna) && mapear == true)
+                    {
+                        //Get the value of the row according to the field name
+                        //Remember that the classïs members and the tableïs field names
+                        //must be identical
+                        object columnvalue = row[nomecoluna];
+                        //Know check if the value is null. 
+                        //If not, it will be added to the instance
+                        if (columnvalue != DBNull.Value)
+                        {
+                            //Set the value dinamically. Now you need to pass as an argument
+                            //an instance class of the generic class. This instance has been
+                            //created with Activator.CreateInstance(t)
+                            //prop.SetValue(defaultInstance, columnvalue, null);
+                            try
+                            {
+                                prop.SetValue(defaultInstance, columnvalue, null);
+                            }
+                            catch (Exception)
+                            {
+
+
+                                if (prop.PropertyType == typeof(bool))
+                                {
+                                    prop.SetValue(defaultInstance, Convert.ToBoolean(columnvalue), null);
+                                }
+                                else if (prop.PropertyType == typeof(int))
+                                {
+                                    prop.SetValue(defaultInstance, Convert.ToInt32(columnvalue), null);
+                                }
+                                else if (prop.PropertyType == typeof(double))
+                                {
+                                    prop.SetValue(defaultInstance, Convert.ToDouble(columnvalue), null);
+                                }
+                                else if (prop.PropertyType == typeof(decimal))
+                                {
+                                    prop.SetValue(defaultInstance, Convert.ToDecimal(columnvalue), null);
+                                }
+                                else if (prop.PropertyType == typeof(float))
+                                {
+                                    prop.SetValue(defaultInstance, float.Parse(columnvalue.ToString()), null);
+                                }
+
+                            }
+                        }
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(prop.Name + ": " + ex.ToString());
+                    //return null;
+                }
+            }
+
+            //Now, create a class of the same type of the generic class. 
+            //Then a conversion itïs done to set the value
+            T myclass = (T)defaultInstance;
+            //Add the generic instance to the generic list
+            return myclass;
+        }
+
+        /// <summary>
+        /// Função responsável por mapear um DataTable para uma Lista de objetos
+        /// </summary>
+        /// <typeparam name="T">Type/Class para o qual sera mapeado</typeparam>
+        /// <param name="dataTable">Fonte dos dados</param>
+        /// <returns></returns>
+        public List<T> DataTableToList<T>(DataTable dataTable)
+        {
+            //This create a new list with the same type of the generic class
+            List<T> genericList = new List<T>();
+            //Obtains the type of the generic class
+            Type t = typeof(T);
+
+            //Obtains the properties definition of the generic class.
+            //With this, we are going to know the property names of the class
+            PropertyInfo[] pi = t.GetProperties();
+
+            //For each row in the datatable
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                //Create a new instance of the generic class
+                object defaultInstance = Activator.CreateInstance(t);
+                //For each property in the properties of the class
+
+                var nomecoluna = "";
+                var mapear = true;
+                foreach (PropertyInfo prop in pi)
+                {
+                    try
+                    {
+
+                        nomecoluna = "";
+                        mapear = true;
+
+                        System.Attribute[] attrs = System.Attribute.GetCustomAttributes(prop);//captura todos os custom Atributes
+
+                        //percorre os atributos
+                        foreach (System.Attribute attr in attrs)
+                        {
+                            if (attr is DbMap)
+                            {
+                                DbMap a = (DbMap)attr;
+                                if (a.Map == true)
+                                {
+                                    nomecoluna = a.Coluna;
+                                }
+                                else
+                                {
+                                    nomecoluna = "";
+                                    mapear = false;
+                                }
+                            }
+                        }
+
+                        if (nomecoluna == "")
+                        {
+                            nomecoluna = prop.Name;
+                        }
+                        //Verifica se a coluna existe na DataTable
+                        if (dataTable.Columns.Contains(nomecoluna) && mapear == true)
+                        {
+                            //Get the value of the row according to the field name
+                            //Remember that the classïs members and the tableïs field names
+                            //must be identical
+                            object columnvalue = row[nomecoluna];
+                            //Know check if the value is null. 
+                            //If not, it will be added to the instance
+                            if (columnvalue != DBNull.Value)
+                            {
+                                //Set the value dinamically. Now you need to pass as an argument
+                                //an instance class of the generic class. This instance has been
+                                //created with Activator.CreateInstance(t)
+                                //prop.SetValue(defaultInstance, columnvalue, null);
+                                try
+                                {
+                                    prop.SetValue(defaultInstance, columnvalue, null);
+                                }
+                                catch (Exception)
+                                {
+
+
+                                    if (prop.PropertyType == typeof(bool))
+                                    {
+                                        prop.SetValue(defaultInstance, Convert.ToBoolean(columnvalue), null);
+                                    }
+                                    else if (prop.PropertyType == typeof(int))
+                                    {
+                                        prop.SetValue(defaultInstance, Convert.ToInt32(columnvalue), null);
+                                    }
+                                    else if (prop.PropertyType == typeof(double))
+                                    {
+                                        prop.SetValue(defaultInstance, Convert.ToDouble(columnvalue), null);
+                                    }
+                                    else if (prop.PropertyType == typeof(decimal))
+                                    {
+                                        prop.SetValue(defaultInstance, Convert.ToDecimal(columnvalue), null);
+                                    }
+                                    else if (prop.PropertyType == typeof(float))
+                                    {
+                                        prop.SetValue(defaultInstance, float.Parse(columnvalue.ToString()), null);
+                                    }
+
+                                }
+                            }
+                        }
+
+
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(prop.Name + ": " + ex.ToString());
+                        //return null;
+                    }
+                }
+                //Now, create a class of the same type of the generic class. 
+                //Then a conversion itïs done to set the value
+                T myclass = (T)defaultInstance;
+                //Add the generic instance to the generic list
+                genericList.Add(myclass);
+            }
+            //At this moment, the generic list contains all de datatable values
+            return genericList;
+        }
+    }
+}
---
+++ LSM.Generic/WepApi/Client/ApiClient.cs
@@ -0,0 +1,130 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSM.Generic.WepApi.Client
+{
+    public class ApiClient
+    {
+        private static string AcessToken = "";
+        private static DateTime TokenExpiraEm = DateTime.Now;
+        private static string UserName = "";
+        private static string Password = "";
+
+        public static void SetUserPassword(string UserName, string Password)
+        {
+            ApiClient.UserName = UserName;
+            ApiClient.Password = Password;
+        }
+
+        private class TokenInfo
+        {
+            public string access_token { get; set; }
+            public string token_type { get; set; }
+            public int expires_in { get; set; }
+        }
+
+        /// <summary>
+        /// Monta o client e já faz a autenticação se necessário
+        /// </summary>
+        /// <param name="CaminhoRestService">Dominio onde esta hospedado o WebApi</param>
+        /// <param name="AuthenticationPath">Caminho para autenticação</param>
+        /// <returns>System.Net.Http.HttpClient</returns>
+        public async Task<System.Net.Http.HttpClient> GetClient(string CaminhoRestService, string AuthenticationPath)
+        {
+            try
+            {
+                var client = new System.Net.Http.HttpClient();
+                client.MaxResponseContentBufferSize = 256000;
+
+                if (AcessToken != "" && DateTime.Now < TokenExpiraEm)
+                {
+                    client.DefaultRequestHeaders.Add("Authorization", "Bearer " + AcessToken);
+                }
+                else
+                {
+                    var url = string.Format(CaminhoRestService + AuthenticationPath);
+                    //Remover
+                    UserName = "leonardo@evoluaeducacao.com.br";
+                    Password = "WS";
+
+                    var content = new FormUrlEncodedContent(new[]
+                    {
+                        new KeyValuePair<string, string>("grant_type", "password"),
+                        new KeyValuePair<string, string>("username", UserName),
+                        new KeyValuePair<string, string>("password", Password)
+                    });
+
+                    var resp = await client.PostAsync(url, content);
+
+                    if (resp.IsSuccessStatusCode)
+                    {
+                        var result = JsonConvert.DeserializeObject<TokenInfo>(resp.Content.ReadAsStringAsync().Result);
+
+                        AcessToken = result.access_token;
+                        TokenExpiraEm = DateTime.Now.AddSeconds(result.expires_in);
+
+                        return await this.GetClient(CaminhoRestService, AuthenticationPath);
+                    }
+                    else
+                    {
+                        throw new Exception(resp.ReasonPhrase);
+                    }
+                }
+
+                return client;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+
+        /// <summary>
+        /// PostAsync passando o Parametro por json ( StringContent ) e retornando Entidade
+        /// </summary>
+        /// <typeparam name="Entidade">Type a ser retornando pela chamada</typeparam>
+        /// <param name="CaminhoRestService">Dominio onde esta hospedado o WebApi</param>
+        /// <param name="UrlPath">Caminho do método</param>
+        /// <param name="AuthenticationPath">Caminho para autenticação</param>
+        /// <param name="Parametro">Objecto que sera enviado como parametro</param>
+        /// <returns>Entidade passada</returns>
+        public async Task<Entidade> PostAsync<Entidade>(string CaminhoRestService, string UrlPath, string AuthenticationPath, object Parametro = null)
+        {
+            try
+            {
+                using (var client = await new LSM.Generic.WepApi.Client.ApiClient().GetClient(CaminhoRestService, AuthenticationPath))
+                {
+                    var url = string.Format(CaminhoRestService + UrlPath);
+
+                    var json = JsonConvert.SerializeObject(Parametro);
+                    var content = new System.Net.Http.StringContent(json, Encoding.UTF8, "application/json");
+
+                    var resp = await client.PostAsync(url, content);
+
+                    if (resp.IsSuccessStatusCode)
+                    {
+                        var result = JsonConvert.DeserializeObject<Entidade>(resp.Content.ReadAsStringAsync().Result);
+
+                        return result;
+                    }
+                    else
+                    {
+                        throw new Exception(resp.ReasonPhrase);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+    }
+}
