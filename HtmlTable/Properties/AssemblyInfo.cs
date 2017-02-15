015ceaa33c9e4e8194158caca88d49bca5c4540f
Leonardo Silvio de Moura <leonardosimoura@gmail.com>
2017-02-14 22:00:39 -0200

    commit

--- HtmlTable/Properties/AssemblyInfo.cs
+++ HtmlTable/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("HtmlTable")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("HtmlTable")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("LSM")]
 [assembly: AssemblyProduct("HtmlTable")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("LSM")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
